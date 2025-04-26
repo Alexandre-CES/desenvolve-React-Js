c8fe47711ac450aa428f62b95e024f38e8cbb3c7
Alexandre <alexandreces666@gmail.com>
2025-04-26 20:30:45 -0300

    component inside component

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,9 +1,27 @@
 import React from "react"
 
-const Curso = (props) => {
+const Professor = (props) => {
   return(
     <div>
-      <h2>Curso de {props.nome} ({props.carga}h)</h2>
+      <Geral nome={props.nome} especialidade={props.especialidade} idade={props.idade}/>
+      <Social urlFacebook={props.facebook} urlInstagram={props.instagram}/>
+      <hr/>
+    </div>
+  )
+}
+
+const Geral = (props) => {
+  return(
+    <div>
+      <h2>{props.nome} | {props.especialidade} | {props.idade}</h2>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <a href={props.urlFacebook}>Facebook</a> - <a href={props.urlInstagram}>Instagram</a>
     </div>
   )
 }
@@ -11,10 +29,10 @@ const Curso = (props) => {
 function App(){
   return(
     <div>
-      <h1>Bem Vindo ao sistema!</h1>
-      <Curso nome='React' carga='30'/>
-      <Curso nome='Java' carga='16'/>
-      <Curso nome='PHP' carga='18'/>
+      <h1>Conheça nossos professores</h1>
+      <Professor nome='Lucas' especialidade='Front-end' idade='37' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
+      <Professor nome='Pedro' especialidade='Back-end' idade='27' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
+      <Professor nome='Robson' especialidade='Fullstack' idade='25' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
     </div>
   );
 }
