96b90f30fc4d9d53ba112524e2eee8653a24566c
Alexandre <alexandreces666@gmail.com>
2025-04-26 11:59:12 -0300

    components and props

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,7 +1,22 @@
-export default function App(){
+import React from "react"
+
+const Curso = (props) => {
   return(
     <div>
-      <h1>Bem Vindo ao sistema!</h1>
+      <h2>Curso de {props.nome} ({props.carga}h)</h2>
     </div>
   )
 }
+
+function App(){
+  return(
+    <div>
+      <h1>Bem Vindo ao sistema!</h1>
+      <Curso nome='React' carga='30'/>
+      <Curso nome='Java' carga='16'/>
+      <Curso nome='PHP' carga='18'/>
+    </div>
+  );
+}
+
+export default App;
