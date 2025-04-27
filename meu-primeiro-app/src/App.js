9504ec203f31fd039d0e2828d4f2551ff7c6b0c8
Alexandre <alexandreces666@gmail.com>
2025-04-27 17:07:52 -0300

    state

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,47 +1,47 @@
 import React, {Component} from "react"
 
-class Professor extends Component{
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Lucas',
+      contador: 0
+    };
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar(){
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
+  }
+
+  diminuir(){
+    let state = this.state;
+
+    if(this.state.contador === 0){
+      alert('valor não pode ser menor que zero');
+      return
+    }else{
+      state.contador -= 1;
+      this.setState(state);
+    }
+  }
+
   render(){
     return(
       <div>
-        <Geral nome={this.props.nome} especialidade={this.props.especialidade} idade={this.props.idade}/>
-        <Social urlFacebook={this.props.facebook} urlInstagram={this.props.instagram}/>
-        <hr/>
+        <h1>Contador</h1>
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
       </div>
-    );
-  };
-}
-
-class Geral extends Component{
-  render(){
-    return(
-      <div>
-        <h2>{this.props.nome} | {this.props.especialidade} | {this.props.idade}</h2>
-      </div>
-    );
-  };
-}
-
-class Social extends Component{
-  render(){
-    return(
-      <div>
-        <a href={this.props.urlFacebook}>Facebook</a> - <a href={this.props.urlInstagram}>Instagram</a>
-      </div>
-    );
-  };
-}
-
-function App(){
-
-  return (
-    <div>
-      <h1>Conheça nossos professores</h1>
-      <Professor nome='Lucas' especialidade='Front-end' idade='37' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
-      <Professor nome='Pedro' especialidade='Back-end' idade='27' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
-      <Professor nome='Robson' especialidade='Fullstack' idade='25' facebook='https://www.facebook.com' instagram='https://www.instagram.com'/>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
