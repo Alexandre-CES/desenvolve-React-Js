3a88ac96ebd2e05f0ff80215f7437625755dd4a0
Alexandre <alexandreces666@gmail.com>
2025-04-28 17:49:18 -0300

    list map

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -4,41 +4,65 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      logado:false
+      feed:[
+        {
+          id:1,
+          username:'Lucas',
+          likes:10, 
+          comments:3
+        },
+        {
+          id:2,
+          username:'Robson',
+          likes:25, 
+          comments:6
+        },
+        {
+          id:3,
+          username:'Douglas',
+          likes:52, 
+          comments:9
+        },
+        {
+          id:4,
+          username:'Pedro',
+          likes:1, 
+          comments:1
+        },
+      ]
     };
-
-    this.entrar = this.entrar.bind(this);
-    this.sair = this.sair.bind(this);
-  }
-
-  entrar(){
-    this.setState({logado:true});
-  }
-
-  sair(){
-    this.setState({logado:false});
   }
 
   render(){
     return(
       <div>
-        <h1>Curso de React JS</h1>
-        <div>
-          {
-            (this.state.logado) ? 
-            <div>
-              <h2>Bem-vindo ao sistema!</h2>
-              <button onClick={this.sair}>Sair</button>
-            </div>:
-            <div>
-              <h2>Faça o login</h2>
-              <button onClick={this.entrar}>Realizar Login</button>
-            </div>
-          }
-        </div>
+        {
+          this.state.feed.map((item)=>{
+            return(
+              <div key={item.id}>
+                <h3>{item.username}</h3>
+                <a>
+                  {
+                    item.likes > 1 ? 
+                    item.likes + ' curtidas' : 
+                    item.likes + ' curtida'
+                  }
+                </a> 
+                 \ 
+                <a>
+                  {
+                    item.comments > 1 ? 
+                    item.likes + ' comentários' :
+                    item.comments + ' comentário'
+                  }
+                </a>
+                <hr/>
+              </div>
+            );
+          })
+        }
       </div>
-      
-    )
+    );
   }
 }
 
