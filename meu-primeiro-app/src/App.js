360218e35f27e43508838c17292645246bf97b49
Alexandre <alexandreces666@gmail.com>
2025-04-28 17:29:20 -0300

    conditional render

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,20 +1,43 @@
 import React, {Component} from "react"
-import Membro from "./Components/Membro";
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      nome:''
+      logado:false
     };
+
+    this.entrar = this.entrar.bind(this);
+    this.sair = this.sair.bind(this);
+  }
+
+  entrar(){
+    this.setState({logado:true});
+  }
+
+  sair(){
+    this.setState({logado:false});
   }
 
   render(){
     return(
       <div>
-        <Membro nome='Visitante'/>
-        
+        <h1>Curso de React JS</h1>
+        <div>
+          {
+            (this.state.logado) ? 
+            <div>
+              <h2>Bem-vindo ao sistema!</h2>
+              <button onClick={this.sair}>Sair</button>
+            </div>:
+            <div>
+              <h2>Faça o login</h2>
+              <button onClick={this.entrar}>Realizar Login</button>
+            </div>
+          }
+        </div>
       </div>
+      
     )
   }
 }
