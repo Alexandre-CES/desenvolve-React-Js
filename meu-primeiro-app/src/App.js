1f437e8facbe45fd3fbd3fd075e91971572d8684
Alexandre <alexandreces666@gmail.com>
2025-05-06 18:10:56 -0300

    hooks

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,52 +1,37 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      form:{
-        email:'teste@gmail.com',
-        senha:'123456',
-        sexo:''
-      }
+function App(){
+
+  const [tarefas,setTarefas] = useState([
+    'Fazer compras',
+    'Lavar Roupas'
+  ])
+
+  const [input,setInput] = useState('');
+
+  function adicionar(){
+    setTarefas([...tarefas,input]);
+
+    setInput('');
+  }
+
+  return(
+    <div>
+      <h1>Hooks</h1>
       
-    };
+      <ul>
+        {
+          tarefas.map((tarefa)=>{
+            return <li>{tarefa}</li>
+          })
+        }
+      </ul>
 
-    this.changeForm = this.changeForm.bind(this);
-  }
+      <input type='text' value={input} onChange={(e)=>{setInput(e.target.value)}}></input>
+      <button type='button' onClick={adicionar}>Adicionar</button>
 
-  changeForm(e){
-    let form = this.state.form;
-    form[e.target.name] = e.target.value;
-    this.setState({form:form});
-  }
-
-  render(){
-    return(
-      <div>
-        <h1>Tela de login</h1>
-        <div>
-          <label>Email:</label>
-          <input type='email' name='email' value={this.state.form.email} onChange={this.changeForm}/>
-        </div>
-        <div>
-          <label>Senha:</label>
-          <input type='password' name='senha' value={this.state.form.senha} onChange={this.changeForm}/>
-        </div>
-        <div>
-          <label>Sexo:</label>
-          <select name='sexo' value={this.state.form.sexo} onChange={this.changeForm}>
-            <option value='m'>masculino</option>
-            <option value='f'>feminino</option>
-          </select>
-        </div>
-
-        <h2>{this.state.form.email}</h2>
-        <h2>{this.state.form.senha}</h2>
-        <h2>{this.state.form.sexo}</h2>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
