1f9bb6e0e5fd66433175194f870f5d2d02d4f2a9
Alexandre <alexandreces666@gmail.com>
2025-05-08 19:54:51 -0300

    http request and style

--- meu-primeiro-app/src/App.js
+++ meu-primeiro-app/src/App.js
@@ -1,52 +1,40 @@
-import React, {useState, useEffect, useMemo, useCallback} from "react"
+import React, {useState, useEffect} from "react";
+import './style.css';
 
 function App(){
 
-  const [tarefas,setTarefas] = useState([])
+  const [posts,setPosts] = useState([]);
 
-  const [input,setInput] = useState('');
-
-  //component didMount
   useEffect(()=>{
-    const tarefasStorage = localStorage.getItem('tarefas');
-
-    if(tarefasStorage){
-      setTarefas(JSON.parse(tarefasStorage));
+    function loadApi(){
+      let url = 'https://jsonplaceholder.typicode.com/posts';
+      fetch(url)
+        .then(result => result.json())
+        .then(json => {
+          setPosts(json);
+        })
     }
+
+    loadApi();
   },[])
 
-  //component didUpdate
-  useEffect(()=>{
-    if(tarefas.length != 0){
-      localStorage.setItem('tarefas',JSON.stringify(tarefas));
-    } 
-  }, [tarefas])
-
-  const adicionar = useCallback(() =>{
-    setTarefas([...tarefas,input]);
-
-    setInput('');
-  }, [input,tarefas]);
-
-  const totalTarefas = useMemo(()=>tarefas.length, [tarefas])
-
   return(
-    <div>
-      <h1>Hooks</h1>
+    <div className="container">
+      <header>
+        <strong>React Blog</strong>
+      </header>
+
+      {
+        posts.map((post)=>{
+          return(
+            <article key={post.id} className="post">
+              <strong className="title">{post.title}</strong>
+              <p>{post.body}</p>
+            </article>
+          )
+        })
+      }
       
-      <ul>
-        {
-          tarefas.map((tarefa)=>{
-            return <li key={tarefa}>{tarefa}</li>
-          })
-        }
-      </ul>
-
-      <h2>Você tem {totalTarefas} tarefas pendentes</h2>
-
-      <input type='text' value={input} onChange={(e)=>{setInput(e.target.value)}}></input>
-      <button type='button' onClick={adicionar}>Adicionar</button>
-
     </div>
   );
 }
